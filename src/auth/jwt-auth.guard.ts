55f845cad4dcf8e1d055c824bde6a1f7e72edca0
Riri-Z <riri.zamor@gmail.com>
2024-11-05 01:11:44 +0100

    Fix :  fix jwt authorization

--- src/auth/jwt-auth.guard.ts
+++ src/auth/jwt-auth.guard.ts
@@ -10,12 +10,16 @@ export class JwtAuthGuard extends AuthGuard('jwt') {
   canActivate(context: ExecutionContext) {
     const request = context.switchToHttp().getRequest();
 
-    const accessToken: string = request?.cookies?.accessToken;
+    const authorization: string | undefined = request?.headers?.authorization;
+    const accessToken: string | undefined = request?.cookies?.accessToken;
 
-    if (!accessToken) {
+    if (!accessToken && !authorization) {
       throw new UnauthorizedException('Wrong accessToken');
     }
-    request.headers.authorization = `Bearer ${accessToken}`;
+
+    request.headers.authorization = accessToken
+      ? `Bearer ${accessToken}`
+      : authorization;
 
     return super.canActivate(context);
   }
